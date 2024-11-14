077fee095bba9488d0d3eedd97b63d71f00f35bd
Islam <islammohamedd1@gmail.com>
2024-11-14 15:56:38 +0200

    docker setup and router updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import EventsList from "./components/EventsList";
 import EventDetails from "./components/EventDetails";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<EventsList />} />
-        <Route path="/events/:id" element={<EventDetails />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <EventsList />,
+    },
+    {
+      path: "/events/:id",
+      element: <EventDetails />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
